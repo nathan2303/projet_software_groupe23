7b694158edc2e664b50850a760d5453d1b0151c7
Quentin Nativel <Quentin Nativel@DESKTOP-88LK7UN>
2017-05-04 16:19:15 +0200

    hi

---
+++ Group23_Projet_IS1220_part1_Nativel_Vermeersch/GUI_Manager/MainInterface.java
@@ -0,0 +1,13 @@
+package GUI_Manager;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainInterface {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton users = new JButton("gérer les utilisateurs");
+	JButton changePrice = new JButton("changer les prix");
+	JButton stats = new JButton("statistiques du système");
+}
---
+++ Group23_Projet_IS1220_part1_Nativel_Vermeersch/GUI_Manager/Statistiques.java
@@ -0,0 +1,14 @@
+package GUI_Manager;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Statistiques {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton close = new JButton("revenir au menu princinpal");
+//	JLabel lab = new jla
+
+}
